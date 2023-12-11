cffb844a52837d0ee0d195ace5cf46274c847baa
KrisLiu <1131747509@qq.com>
2023-12-11 22:36:59 +0800

    加入sql文件

---
+++ bubble/users.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users`(`id`, `created_at`, `updated_at`, `deleted_at`, `name`, `age`) VALUES (1, '2023-11-30 12:22:46', '2023-11-30 12:22:46', NULL, 'qimi', 18);
+INSERT INTO `users`(`id`, `created_at`, `updated_at`, `deleted_at`, `name`, `age`) VALUES (2, '2023-11-30 12:22:46', '2023-11-30 12:22:46', NULL, 'jinzhu', 20);
